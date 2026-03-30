65888b1c380a65e491e8f208dac1459f3726cc8f
douglasgvr <douglasfarmaangra@gmail.com>
2026-03-30 11:10:40 -0300

    feat: adiciona lógica para criação de cofre e navegação para o dashboard após sucesso

--- src/app/pages/setup-conta/setup-conta.ts
+++ src/app/pages/setup-conta/setup-conta.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-setup-conta',
@@ -7,13 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './setup-conta.css',
 })
 export class SetupConta {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
   criarCofre() {
     const dadosSalvos = localStorage.getItem('usuarioLogado');
 
     if (dadosSalvos) {
       const usuario = JSON.parse(dadosSalvos);
       const meuId = usuario.id;
-      console.log('Botao clicado, ID do usuário:', meuId);
+
+      const pacoteParaOJava = {
+        nome: 'Cofre da Família',
+        usuarioId: meuId,
+      };
+
+      console.log('Pacote para o Java:', pacoteParaOJava);
+
+      this.http.post('http://localhost:8080/api/contas', pacoteParaOJava).subscribe({
+        next: (respostaNovaConta: any) => {
+          console.log('Sucesso ao criar o cofre:', respostaNovaConta);
+          usuario.contaCasal = respostaNovaConta;
+          localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+          this.router.navigate(['/dashboard']);
+        },
+        error: (erro) => {
+          console.error('Erro ao criar o cofre:', erro);
+        },
+      });
     } else {
       console.log('Nenhum usuário logado encontrado.');
     }
