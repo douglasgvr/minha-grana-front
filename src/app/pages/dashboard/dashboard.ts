9d69c534b4dcaba41f517f59b5b438f9d1c055b8
douglasgvr <douglasfarmaangra@gmail.com>
2026-03-31 09:55:54 -0300

    fix: ajusta payload da transacao e finaliza integracao de receitas e despesas no dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CurrencyPipe, CommonModule],
+  imports: [CurrencyPipe, CommonModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -24,6 +25,12 @@ export class Dashboard implements OnInit {
     totalReceitas: 0,
     totalDespesas: 0,
     saldoAtual: 0,
+  };
+
+  novaTransacao = {
+    tipo: 'RECEITA',
+    valor: null,
+    descricao: '',
   };
 
   constructor(private http: HttpClient) {}
@@ -48,6 +55,41 @@ export class Dashboard implements OnInit {
         console.error('Nenhum usuário logado encontrado.');
       }
     }
+  }
+
+  salvarTransacao() {
+    const dadosSalvos = localStorage.getItem('usuarioLogado');
+    let meuId = '';
+    if (dadosSalvos) {
+      const usuario = JSON.parse(dadosSalvos);
+      meuId = usuario.id;
+    }
+
+    const pacoteParaOJava = {
+      descricao: this.novaTransacao.descricao,
+      valor: this.novaTransacao.valor,
+      tipoTransacao: this.novaTransacao.tipo,
+      dataPagamento: new Date().toISOString().split('T')[0], // Pega a data de hoje no formato YYYY-MM-DD
+      despesaConjunta: true,
+      usuarioId: meuId,
+      contaCasalId: this.contaCasalId,
+    };
+
+    console.log('Enviando transação corrigida para a cozinha...', pacoteParaOJava);
+
+    this.http.post('http://localhost:8080/api/transacoes', pacoteParaOJava).subscribe({
+      next: (resposta) => {
+        console.log('Sucesso! O Java salvou a transação:', resposta);
+        this.fecharModal();
+        this.novaTransacao = { descricao: '', valor: null, tipo: 'DESPESA' };
+
+        this.buscarResumoNoBackend();
+      },
+      error: (erro) => {
+        console.error('Erro ao salvar a transação:', erro);
+        alert('Erro ao salvar. Olhe o console (F12) para ver o que o Java reclamou.');
+      },
+    });
   }
 
   buscarResumoNoBackend() {
